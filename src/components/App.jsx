f41ce5e5e892004f612323bc9ae7f0bed4b8179c
Student <example@example.com>
2019-07-01 15:08:12 -0700

    fix global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,37 @@ import Feed from './Feed';
 import SuggestionBox from './SuggestionBox';
 
 function App(){
+  //
+  // let styling = {
+  //   display: 'grid',
+  //   gridTemplateColumns: '1fr 1fr 1fr'
+  // }
+  return (<div>
+    <style jsx global>{`
 
-  let styling = {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr'
-  }
-  return <div><Header/><div style = {styling}><div><Profile name= 'Barack Obama' img='https://picsum.photos/80/80' bgImg='https://picsum.photos/200/300'/>
-    <About text ='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy
-       text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        It has survived not only five centuries, but also the leap into electroni'/></div><Feed/><SuggestionBox/></div></div>
-  ;
+        button {
+          background-color: blue;
+          border-radius: 10px;
+          color: white;
+        }
+        .main-grid {
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+        }
+
+    `}
+    </style>
+    <Header/>
+    <div className="main-grid">
+      <div>
+        <Profile name= 'Barack Obama' img='https://picsum.photos/80/80' bgImg='https://picsum.photos/200/300'/>
+        <About text ='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electroni'/>
+      </div>
+      <Feed/>
+      <SuggestionBox/>
+    </div>
+  </div>
+  );
 }
 
 export default App;
